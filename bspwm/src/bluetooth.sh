a50ef964f0da268ae5d96c4ea114b4a16e229453
mddr4g00n25 <jorgedonairesmendoza25@gmail.com>
2024-08-02 17:35:03 -0500

    ptmr

---
+++ bspwm/src/bluetooth.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+read -r current_rice < "$HOME"/.config/bspwm/.rice
+
+# Colors
+DIR="$HOME/.config/bspwm/rices/${current_rice}/config.ini"
+POWER_ON=`cat $DIR | grep -m 1 '^blue =' | sed -n 's/.*= \(#[0-9a-fA-F]\+\).*/\1/p'`
+POWER_OFF=`cat $DIR | grep -m 1 '^grey =' | sed -n 's/.*= \(#[0-9a-fA-F]\+\).*/\1/p'`
+
+# Checks if bluetooth controller is powered on
+power_on() {
+    if bluetoothctl show | grep -q "Powered: yes"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+print_status() {
+    if power_on; then
+		echo "%{F$POWER_ON}󰂯%{F-}"
+    else
+        echo "%{F$POWER_OFF}󰂲%{F-}"
+    fi
+}
+
+print_status
